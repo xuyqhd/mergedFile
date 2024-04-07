1a7b382a8c5f756f9851a76d06d3e4357dbd813f
xuyqhd <1824002737@qq.com>
2024-04-07 12:27:54 +0800

    编译成功

--- mergedFile.c
+++ mergedFile.c
@@ -1,8 +1,8 @@
 /*
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2024-03-25 09:46:01
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2024-03-25 13:17:03
+ * @LastEditors: xuyqhd 1824002737@qq.com
+ * @LastEditTime: 2024-03-25 14:07:38
  * @FilePath: \mergedFile\mergedFile.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
